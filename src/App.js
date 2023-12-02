abcb5f5bdb8b21b60b6e8ca86538b8032d9b9612
Deepak <deepamdeebu@gmail.com>
2023-12-02 20:14:35 +0530

    Varibales : Adding some variables into the component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 
 function App() {
+  const headding = 'Sample Componenet 2';
+  const text = 'This is a sample paragraph';
+  const number = 5;
+  const sample_array = [1, 2, 3, 4, 5];
+  const link = "https://www.google.com/";
   return (
     <div className="App">
       <div className="Component">
-        <h1>Sample Component</h1>
+        <h1>{ headding }</h1>
+        <p>{ text } { number }</p>
+        <p>{ sample_array }</p>
+        <a href={ link }>{ 'Sample link' }</a>
       </div>
     </div>
   );
