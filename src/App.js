1b44d367c2cee455422b6ba1358386342129651b
Deepak <deepamdeebu@gmail.com>
2023-12-02 20:29:37 +0530

    Components : Adding child components into the main app component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const headding = 'Sample Componenet 2';
-  const text = 'This is a sample paragraph';
-  const number = 5;
-  const sample_array = [1, 2, 3, 4, 5];
-  const link = "https://www.google.com/";
   return (
     <div className="App">
+      <Navbar />
       <div className="Component">
-        <h1>{ headding }</h1>
-        <p>{ text } { number }</p>
-        <p>{ sample_array }</p>
-        <a href={ link }>{ 'Sample link' }</a>
+        <Home />
       </div>
     </div>
   );
