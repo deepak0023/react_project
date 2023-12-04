51a7daf9285ce00742fd21c983176d7245e9d771
deepak <deepamdeebu@gmail.com>
2023-12-04 19:06:38 +0530

    Blog Details : Getting value of each item and displaying data based on id in blog details page

--- src/ItemDetails.js
+++ src/ItemDetails.js
@@ -1,11 +1,21 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const ItemDetails = () => {
   const { id } = useParams();
+  const { data: item, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
 
   return (
-    <div className="blog-details">
-      <h2>Item details - { id }</h2>
+    <div className="item-details">
+      { isPending && <div>Loading...</div> }
+      { error && <div>{ error }</div> }
+      { item && (
+        <article>
+          <h2>{ item.title }</h2>
+          <p>Written by { item.author }</p>
+          <div>{ item.body }</div>
+        </article>
+      )}
     </div>
   );
 }
