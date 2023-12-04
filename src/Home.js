b74e6272f29b6494366202e7628940bf2847a0e4
deepak <deepamdeebu@gmail.com>
2023-12-04 16:42:29 +0530

    Featch : Fetch the data from the backend api and display it

--- src/Home.js
+++ src/Home.js
@@ -2,29 +2,26 @@ import { useEffect, useState } from "react";
 import ItemList from "./ItemList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'student_1', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'student_2', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'student_1', id: 3 }
-  ])
-
-  const [name, setName] = useState('student_5');
+  const [blogs, setBlogs] = useState(null);
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBlogs(newBlogs);
   }
 
-  useEffect(() => {                  // If no second parameter then run on every render
-    console.log('use effect ran');
-    console.log(blogs);
-  }, [name])                         // empty array means only on first render
+  useEffect(() => {
+    fetch('http://localhost:8000/blogs')
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        setBlogs(data);
+      })
+  }, [])
 
   return (
     <div className="home">
-      <ItemList items={blogs} title="All Blogs" handleDelete={handleDelete}/>
-      <button onClick={() => setName('Student_6')}>change name</button>
-      <p>{name}</p>
+      {blogs && <ItemList items={blogs} handleDelete={handleDelete} />}
     </div>
   );
 }
