33c248e0a2414ea839581b097286fe7147943977
deepak <deepamdeebu@gmail.com>
2023-12-04 17:17:20 +0530

    Custom Hooks : Move the fetch functionality into resuable custom hook

--- src/Home.js
+++ src/Home.js
@@ -1,31 +1,9 @@
 import { useEffect, useState } from "react";
 import ItemList from "./ItemList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [isPending, setIsPending] = useState(true);
-  const [blogs, setBlogs] = useState(null);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    setTimeout(() => {             // Added settimeout to view the loading screen
-      fetch('http://localhost:8000/blogs')
-      .then(res => {
-        if (!res.ok) {               // error coming back from server
-          throw Error('could not fetch the data for that resource');
-        }
-        return res.json();
-      })
-      .then(data => {
-        setIsPending(false);
-        setBlogs(data);
-        setError(null);
-      })
-      .catch(err => { // auto catches network / connection error
-        setIsPending(false);
-        setError(err.message);
-      })
-    }, 1000);
-  }, [])
+  const { error, isPending, data: blogs } = useFetch('http://localhost:8000/blogs');
 
   return (
     <div className="home">
@@ -35,5 +13,5 @@ const Home = () => {
     </div>
   );
 }
- 
+
 export default Home;
