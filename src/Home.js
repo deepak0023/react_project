02569c5a2296d8ffe1dee7c11389221e135ec817
deepak <deepamdeebu@gmail.com>
2023-12-04 13:40:36 +0530

    Parsing Objects : Parse javascript object and display in the UI

--- src/Home.js
+++ src/Home.js
@@ -2,19 +2,21 @@ import { useState } from "react";
 
 const Home = () => {
 
-    const [name, setName] = useState('student_1');
-    const [age, setAge] = useState(25);
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ]);
 
-    const handleClick = () => {
-        setName('student_2');
-        setAge(30);
-    }
 
     return (
       <div className="home">
-        <h2>Homepage</h2>
-        <p>{ name } is { age } years old</p>
-        <button onClick={handleClick}>Click me</button>
+        {blogs.map(blog => (
+          <div className="blog-preview" key={blog.id} >
+            <h2>{ blog.title }</h2>
+            <p>Written by { blog.author }</p>
+          </div>
+        ))}
       </div>
     );
 }
