22d2b016d02a675e3dfe98b540354aaec50fc6d6
deepak <deepamdeebu@gmail.com>
2023-12-04 16:54:26 +0530

    Loading : Add loading message to the screen before fetching data

--- src/Home.js
+++ src/Home.js
@@ -2,20 +2,25 @@ import { useEffect, useState } from "react";
 import ItemList from "./ItemList";
 
 const Home = () => {
+  const [isPending, setIsPending] = useState(true);
   const [blogs, setBlogs] = useState(null);
 
   useEffect(() => {
-    fetch('http://localhost:8000/blogs')
+    setTimeout(() => {             // Added settimeout to view the loading screen
+      fetch('http://localhost:8000/blogs')
       .then(res => {
         return res.json();
       })
       .then(data => {
+        setIsPending(false);
         setBlogs(data);
-      })
+      });
+    }, 1000);
   }, [])
 
   return (
     <div className="home">
+      {isPending && <div>Loading...</div>}
       {blogs && <ItemList items={blogs} />}
     </div>
   );
