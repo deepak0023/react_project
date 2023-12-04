dbc2a6727963a575e74fefb740c5584825740113
deepak <deepamdeebu@gmail.com>
2023-12-04 15:09:26 +0530

    Filter : Filter the data object based on param and display using reuseable componenet

--- src/Home.js
+++ src/Home.js
@@ -3,14 +3,15 @@ import ItemList from "./ItemList";
 
 const Home = () => {
   const [blogs, setBlog] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    { title: 'My new website', body: 'lorem ipsum...', author: 'student_1', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'student_2', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'student_1', id: 3 }
   ])
 
   return (
     <div className="home">
       <ItemList items={blogs} title="All Blogs" />
+      <ItemList items={blogs.filter(blog => blog.author === 'student_1' )} title="Student 1 Blogs" />
     </div>
   );
 }
