572e04746e7f97292cb4bf0394a3f1011f72f5d9
deepak <deepamdeebu@gmail.com>
2023-12-04 16:01:02 +0530

    Use Effect : Understanding the useEffect hook in react

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ItemList from "./ItemList";
 
 const Home = () => {
@@ -8,14 +8,23 @@ const Home = () => {
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'student_1', id: 3 }
   ])
 
+  const [name, setName] = useState('student_5');
+
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBlogs(newBlogs);
   }
 
+  useEffect(() => {                  // If no second parameter then run on every render
+    console.log('use effect ran');
+    console.log(blogs);
+  }, [name])                         // empty array means only on first render
+
   return (
     <div className="home">
       <ItemList items={blogs} title="All Blogs" handleDelete={handleDelete}/>
+      <button onClick={() => setName('Student_6')}>change name</button>
+      <p>{name}</p>
     </div>
   );
 }
