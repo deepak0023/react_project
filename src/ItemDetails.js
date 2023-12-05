94047def3e82a32d5af8c6ed350ceeb0826eea4a
deepak <deepamdeebu@gmail.com>
2023-12-05 11:08:04 +0530

    Delete : Add a remove record feature and redirect to the home page

--- src/ItemDetails.js
+++ src/ItemDetails.js
@@ -1,9 +1,18 @@
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const ItemDetails = () => {
   const { id } = useParams();
   const { data: item, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
+  const history = useHistory();
+
+  const handleClick = () => {
+    fetch('http://localhost:8000/blogs/' + item.id, {
+      method: 'DELETE'
+    }).then(() => {
+      history.push('/');
+    })
+  }
 
   return (
     <div className="item-details">
@@ -14,6 +23,7 @@ const ItemDetails = () => {
           <h2>{ item.title }</h2>
           <p>Written by { item.author }</p>
           <div>{ item.body }</div>
+          <button onClick={handleClick}>delete</button>
         </article>
       )}
     </div>
